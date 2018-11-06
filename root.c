bf08ce0a5a466f94cf53c2897111a851747108b0
Konstantina Stoikou <tinasto97@gmail.com>
2018-11-06 12:46:39 +0200

    Read command line arguments

--- root.c
+++ root.c
@@ -1,6 +1,30 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char const *argv[]) {
-    /* code */
+    int height;
+    char *datafile;
+    char *pattern;
+    int skew = 0;       //flag for skew searching (initialized to false)
+
+    //read command line arguments and initialize variables respectively
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-h") == 0) {
+            height = atoi(argv[i+1]);
+        }
+        else if (strcmp(argv[i], "-d") == 0) {
+            datafile = malloc(sizeof(strlen(argv[i+1]) + 1));
+            strcpy(datafile, argv[i+1]);
+        }
+        else if (strcmp(argv[i], "-p") == 0) {
+            pattern = malloc(sizeof(strlen(argv[i+1]) + 1));
+            strcpy(pattern, argv[i+1]);
+        }
+        else if (strcmp(argv[i], "-s") == 0) {
+            skew = 1;
+        }
+    }
+    printf("height: %d, datafile: %s, pattern: %s, skew: %d\n", height, datafile, pattern, skew);
     return 0;
 }
